99770f13b22ec510bebe60bfad31a58b1d072b65
Igor Assis <123173507+NoobAuthor@users.noreply.github.com>
2023-02-16 10:34:31 -0300

    Add files via upload

--- controllers/image.js
+++ controllers/image.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const Clarifai = require("clarifai");
 
 const app = new Clarifai.App({
@@ -38,44 +37,3 @@ module.exports = {
   handleImage,
   handleApiCall,
 };
-=======
-const Clarifai = require("clarifai");
-
-const app = new Clarifai.App({
-  apiKey: "9a6d250feb674a91b724d34bff1fd841",
-});
-
-const handleApiCall = (req, res) => {
-  app.models
-    .predict(
-      {
-        id: "face-detection",
-        name: "face-detection",
-        version: "6dc7e46bc9124c5c8824be4822abe105",
-        type: "visual-detector",
-      },
-      req.body.input
-    )
-    .then((data) => {
-      res.json(data);
-    })
-    .catch((err) => res.status(400).json("unable to work with API"));
-};
-
-const handleImage = (req, res, db) => {
-  const { id } = req.body;
-  db("users")
-    .where("id", "=", id)
-    .increment("entries", 1)
-    .returning("entries")
-    .then((entries) => {
-      res.json(entries[0].entries);
-    })
-    .catch((err) => res.status(400).json("unable to get entries"));
-};
-
-module.exports = {
-  handleImage,
-  handleApiCall,
-};
->>>>>>> 4d3b6ea (Finished the server. Cleaned the code, now separated in the controllers folder.)
--- controllers/profile.js
+++ controllers/profile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const handleProfileGet = (req, res, db) => {
   const { id } = req.params;
   db.select("*")
@@ -17,23 +16,3 @@ const handleProfileGet = (req, res, db) => {
 module.exports = {
   handleProfileGet,
 };
-=======
-const handleProfileGet = (req, res, db) => {
-  const { id } = req.params;
-  db.select("*")
-    .from("users")
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json("Not found");
-      }
-    })
-    .catch((err) => res.status(400).json("error getting user"));
-};
-
-module.exports = {
-  handleProfileGet,
-};
->>>>>>> 4d3b6ea (Finished the server. Cleaned the code, now separated in the controllers folder.)
