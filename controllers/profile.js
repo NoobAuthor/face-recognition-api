19582925bfa4c3ddafe7de3f37352913836a4cb9
Igor Assis <123173507+NoobAuthor@users.noreply.github.com>
2023-02-16 10:30:13 -0300

    Add files via upload

---
+++ controllers/profile.js
@@ -0,0 +1,39 @@
+<<<<<<< HEAD
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+};
+
+module.exports = {
+  handleProfileGet,
+};
+=======
+const handleProfileGet = (req, res, db) => {
+  const { id } = req.params;
+  db.select("*")
+    .from("users")
+    .where({ id })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      } else {
+        res.status(400).json("Not found");
+      }
+    })
+    .catch((err) => res.status(400).json("error getting user"));
+};
+
+module.exports = {
+  handleProfileGet,
+};
+>>>>>>> 4d3b6ea (Finished the server. Cleaned the code, now separated in the controllers folder.)
